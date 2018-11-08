c312b2dc74fbd508eab74ec659272436e12fae4f
Alexander Lichter <manniL@gmx.net>
2018-11-08 17:23:00 +0000

    feat: nuxt 2.2 compat (#2)

--- test/module.test.js
+++ test/module.test.js
@@ -1,70 +1,14 @@
 const consola = require('consola')
 const getPort = require('get-port')
-const { Nuxt, Builder } = require('nuxt-edge')
-
+const nuxtStable = require('nuxt')
+const nuxtEdge = require('nuxt-edge')
 jest.setTimeout(60 * 1000)
 
 let nuxt, port
 
 const url = route => 'http://localhost:' + port + route
 
-describe('nuxt-style-resources', () => {
-  let log
-
-  beforeEach(() => {
-    log = jest.fn()
-    consola.clear().add({ log })
-  })
-
-  describe('scss', () => {
-    test('properly import variables', async () => {
-      nuxt = await setupNuxt(require('./fixture/sass/nuxt.config'))
-
-      const window = await nuxt.server.renderAndGetWindow(url('/'))
-      const headHtml = window.document.head.innerHTML
-      expect(headHtml).toContain('.ymca{color:#333')
-    })
-
-    afterEach(async () => {
-      if (nuxt) {
-        await nuxt.close()
-      }
-    })
-  })
-  describe('stylus', () => {
-    test('properly import variables', async () => {
-      nuxt = await setupNuxt(require('./fixture/stylus/nuxt.config'))
-
-      const window = await nuxt.server.renderAndGetWindow(url('/'))
-      const headHtml = window.document.head.innerHTML
-      expect(headHtml).toContain('.ymca{color:#333')
-    })
-
-    afterEach(async () => {
-      if (nuxt) {
-        await nuxt.close()
-      }
-    })
-  })
-  describe('less', () => {
-    test('properly import variables', async () => {
-      nuxt = await setupNuxt(require('./fixture/less/nuxt.config'))
-
-      const window = await nuxt.server.renderAndGetWindow(url('/'))
-      const headHtml = window.document.head.innerHTML
-      expect(headHtml).toContain('h1{color:#0f0}')
-      expect(headHtml).toContain('.ymca{color:#333')
-    })
-
-    afterEach(async () => {
-      if (nuxt) {
-        await nuxt.close()
-      }
-    })
-  })
-})
-
-const setupNuxt = async (config) => {
+const setupNuxtFn = ({ Nuxt, Builder }) => async (config) => {
   const nuxt = new Nuxt(config)
   await new Builder(nuxt).build()
   port = await getPort()
@@ -72,3 +16,63 @@ const setupNuxt = async (config) => {
 
   return nuxt
 }
+
+describe('nuxt-style-resources', () => {
+  describe.each([['nuxt', nuxtStable], ['nuxt-edge', nuxtEdge]])('%s', (_, nuxtImpl) => {
+    let log
+
+    const setupNuxt = setupNuxtFn(nuxtImpl)
+
+    beforeEach(() => {
+      log = jest.fn()
+      consola.clear().add({ log })
+    })
+
+    describe('scss', () => {
+      test('properly import variables', async () => {
+        nuxt = await setupNuxt(require('./fixture/sass/nuxt.config'))
+
+        const window = await (nuxt.server || nuxt).renderAndGetWindow(url('/'))
+        const headHtml = window.document.head.innerHTML
+        expect(headHtml).toContain('.ymca{color:#333')
+      })
+
+      afterEach(async () => {
+        if (nuxt) {
+          await nuxt.close()
+        }
+      })
+    })
+    describe('stylus', () => {
+      test('properly import variables', async () => {
+        nuxt = await setupNuxt(require('./fixture/stylus/nuxt.config'))
+
+        const window = await (nuxt.server || nuxt).renderAndGetWindow(url('/'))
+        const headHtml = window.document.head.innerHTML
+        expect(headHtml).toContain('.ymca{color:#333')
+      })
+
+      afterEach(async () => {
+        if (nuxt) {
+          await nuxt.close()
+        }
+      })
+    })
+    describe('less', () => {
+      test('properly import variables', async () => {
+        nuxt = await setupNuxt(require('./fixture/less/nuxt.config'))
+
+        const window = await (nuxt.server || nuxt).renderAndGetWindow(url('/'))
+        const headHtml = window.document.head.innerHTML
+        expect(headHtml).toContain('h1{color:#0f0}')
+        expect(headHtml).toContain('.ymca{color:#333')
+      })
+
+      afterEach(async () => {
+        if (nuxt) {
+          await nuxt.close()
+        }
+      })
+    })
+  })
+})
