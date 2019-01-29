fedfe7bcda8c5cabe573bb0da89a587f943d2007
Alexander Lichter <manniL@gmx.net>
2019-01-29 17:42:32 +0100

    test: add test case for nested variable file (#22)

--- test/fixture/sass/nuxt.config.js
+++ test/fixture/sass/nuxt.config.js
@@ -3,12 +3,12 @@ const { resolve } = require('path')
 module.exports = {
   rootDir: resolve(__dirname, '../../../'),
   srcDir: resolve(__dirname),
-  css: ['~assets/a.scss'],
+  css: ['@/assets/a.scss'],
   render: {
     resourceHints: false
   },
   styleResources: {
-    scss: ['./assets/variables.scss']
+    scss: ['@/assets/nested/index.scss']
   },
   modules: ['@@'],
   build: {
