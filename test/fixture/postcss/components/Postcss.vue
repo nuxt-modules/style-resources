bf11e06bcd2596cd4618564e57aa12ce4792d085
Kether Saturnius <iamkether@gmail.com>
2020-07-02 18:25:13 +1000

    test: add postcss test fixtures

---
+++ test/fixture/postcss/components/Postcss.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="post">
+    Test
+  </div>
+</template>
+
+<style lang="postcss">
+  .post {
+    color: $gray;
+  }
+</style>
+
+<style lang="pcss">
+  .post {
+    background-color: $white
+  }
+</style>
---
+++ test/fixture/postcss/nuxt.config.js
@@ -0,0 +1,23 @@
+const { resolve } = require('path')
+
+module.exports = {
+  rootDir: resolve(__dirname, '../../../'),
+  srcDir: resolve(__dirname),
+  css: ['@/assets/a.pcss', '@/assets/a.postcss'],
+  render: {
+    resourceHints: false
+  },
+  styleResources: {
+    pcss: ['@/assets/variables.pcss'],
+    postcss: ['@/assets/variables.postcss']
+  },
+  buildModules: ['@@'],
+  build: {
+    quiet: false,
+    optimization: {
+      splitChunks: {
+        name: true
+      }
+    }
+  }
+}
