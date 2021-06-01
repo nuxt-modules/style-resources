1c993e6c42d19f9e63dbd1d607186ac7a526be28
Rafal Chlodnicki <rchl2k@gmail.com>
2021-06-01 10:25:09 +0200

    chore: no need to transpile for node v12 and up

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,13 @@
 module.exports = {
   env: {
     test: {
-      'plugins': ['@babel/plugin-transform-modules-commonjs']
+      presets: [
+        ['@babel/preset-env', {
+          targets: {
+            node: 'current'
+          }
+        }]
+      ]
     }
   }
 }
