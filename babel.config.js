aec6d7e6ebafd61b656ab02d817bf38eea02d3fd
Rafal Chlodnicki <rchl2k@gmail.com>
2021-06-01 09:25:52 +0200

    chore: fix testing on node 10

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,7 @@
 module.exports = {
   env: {
     test: {
-      presets: [
-        ['@babel/preset-env', {
-          targets: {
-            node: 'current'
-          }
-        }]
-      ]
+      'plugins': ['@babel/plugin-transform-modules-commonjs']
     }
   }
 }
